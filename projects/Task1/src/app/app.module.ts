dd443db239cb2807d88f4ffc47f805aec08d5379
alximFe <alximFe@yandex.ru>
2021-11-30 22:30:28 +0300

    library integrated

--- projects/Task1/src/app/app.module.ts
+++ projects/Task1/src/app/app.module.ts
@@ -3,10 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+import { FormsModule } from '@angular/forms';
+import { GenerateCustomInputsDirective } from './generate-input-directive/generate-input-directive.component';
 import { OnlyNumberComponentComponent } from './only-number-component/only-number-component.component';
 import { OnlyEmailComponentComponent } from './only-email-component/only-email-component.component';
-import { GenerateCustomInputsDirective } from './generate-input-directive/generate-input-directive.component';
-import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
